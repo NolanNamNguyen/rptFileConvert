3f6bddfffa5afd57591d2973f7f4ba36f228bbbe
NolanNamNguyen <nolan.1606.1995@gmail.com>
2020-07-27 19:27:23 +0700

    Initiate Converter

---
+++ RPTFiles_UltimateConverter/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿using System.Windows.Forms;
+
+namespace RPTFiles_UltimateConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.mywork = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(353, 294);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(79, 27);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Convert";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(83, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Drop your file here bro";
+            // 
+            // panel1
+            // 
+            this.panel1.AllowDrop = true;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(232, 77);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(329, 185);
+            this.panel1.TabIndex = 3;
+            this.panel1.DragDrop += new System.Windows.Forms.DragEventHandler(this.panel1_DragDrop);
+            this.panel1.DragEnter += new System.Windows.Forms.DragEventHandler(this.panel1_DragEnter);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(256, 343);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(273, 20);
+            this.progressBar1.TabIndex = 4;
+            this.progressBar1.Maximum = 100;
+            this.progressBar1.Step = 10;
+            this.progressBar1.Value = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "RPTFiles_UltimateConverter";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.ComponentModel.BackgroundWorker mywork;
+        private ProgressBar progressBar1;
+
+    }
+}
+
---
+++ RPTFiles_UltimateConverter/Form1.cs
@@ -0,0 +1,129 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RPTFiles_UltimateConverter
+{
+    public partial class Form1 : Form
+    {
+        private string filename;
+        private string filepath;
+        private string file;
+        private string filenameNoExtension;
+        public Form1()
+        {
+            InitializeComponent();
+            mywork.WorkerReportsProgress = true;
+            // This event will be raised on the worker thread when the worker starts
+            mywork.DoWork += new DoWorkEventHandler(Mywork_OnDoWork);
+            // This event will be raised when we call ReportProgress
+            mywork.ProgressChanged += new ProgressChangedEventHandler(Mywork_OnProgressChanged);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            mywork.RunWorkerAsync();
+        }
+     
+
+        private void Mywork_OnDoWork(object sender, DoWorkEventArgs e)
+        {
+            using (ReportClass rptH = new ReportClass())
+            {
+                CrystalDecisions.Shared.PdfRtfWordFormatOptions pdfOpts = CrystalDecisions.Shared.ExportOptions.CreatePdfRtfWordFormatOptions();
+                CrystalDecisions.Shared.ExcelDataOnlyFormatOptions excelOptsDataOnly = CrystalDecisions.Shared.ExportOptions.CreateDataOnlyExcelFormatOptions();
+                CrystalDecisions.Shared.ExcelFormatOptions excelOpts = CrystalDecisions.Shared.ExportOptions.CreateExcelFormatOptions();
+                CrystalDecisions.Shared.MicrosoftMailDestinationOptions mailOpts = CrystalDecisions.Shared.ExportOptions.CreateMicrosoftMailDestinationOptions();
+                CrystalDecisions.Shared.DiskFileDestinationOptions diskOpts = CrystalDecisions.Shared.ExportOptions.CreateDiskFileDestinationOptions();
+                CrystalDecisions.Shared.ExportOptions exportOpts = new CrystalDecisions.Shared.ExportOptions();
+                ReportDocument rpt = new ReportDocument();
+
+                rpt.Load(file);
+
+                pdfOpts.UsePageRange = false;
+                exportOpts.ExportFormatOptions = pdfOpts;
+
+                excelOptsDataOnly.UseWorksheetFunctionsForSummaries = true;
+                excelOptsDataOnly.MaintainColumnAlignment = true;
+
+
+                string MyRptName = rpt.FileName.ToString();
+                //MyRptName = @"D:\ITProject\Tin\Tin2.xls";
+                MyRptName = filepath + "\\" + filenameNoExtension + ".xls";
+                //diskOpts.DiskFileName = "World Sales Report.pdf";
+                diskOpts.DiskFileName = MyRptName;
+
+                exportOpts.ExportDestinationOptions = diskOpts;
+
+                //exportOpts.ExportDestinationType = CrystalDecisions.Shared.ExportDestinationType.MicrosoftMail;
+                exportOpts.ExportDestinationType = CrystalDecisions.Shared.ExportDestinationType.DiskFile;
+
+                //exportOpts.ExportFormatType = CrystalDecisions.Shared.ExportFormatType.PortableDocFormat;
+                exportOpts.ExportFormatType = CrystalDecisions.Shared.ExportFormatType.ExcelRecord;
+
+                rpt.Export(exportOpts);
+                for (int i = 0; i <= 100; i=i+20)
+                {
+                    // Report progress to 'UI' thread
+                    mywork.ReportProgress(i);
+                    // Simulate long task
+                    System.Threading.Thread.Sleep(100);
+                }
+
+            }
+        }
+
+        private void Mywork_OnProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void Mywork_Onfinish(object sender, RunWorkerCompletedEventArgs e)
+        {
+            // TODO: do something with final calculation.
+        }
+
+
+        void panel1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        void panel1_DragDrop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                var allowedExtensions = new[] {
+                 ".rpt",".RPT"
+                };
+                string[] filess = (string[])e.Data.GetData(DataFormats.FileDrop);
+                file = filess[0];
+                var extension = Path.GetExtension(file);
+                if (allowedExtensions.Contains(extension))
+                {
+                    filename = Path.GetFileName(file);
+                    filepath = Path.GetDirectoryName(file);
+                    filenameNoExtension = Path.GetFileNameWithoutExtension(file);
+                }
+                else
+                {
+                    DialogResult dialog1 = MessageBox.Show("Please choose .rpt file Tin???", "Are you seriously?", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception)
+            {
+                DialogResult dialog = MessageBox.Show("might be something wrong", "Are you seriously?", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                throw;
+            }
+
+        }
+    }
+}
